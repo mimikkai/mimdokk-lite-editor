4052985324c25871ca8ddf6634bd2b7c82d35252
Daniil G <dealenx@gmail.com>
2025-11-20 12:38:11 +0700

    fix: update base path for github pages and switch to npm

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mimdokk-lite-editor/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
